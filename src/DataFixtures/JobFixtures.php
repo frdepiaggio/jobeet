793581918b8cb2f3669f77e084527480de552ab7
frdepiaggio <franco.depi@gmail.com>
2018-07-30 13:27:43 -0300

    Día 10: Cree ABM de Admin

--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -64,23 +64,6 @@ class JobFixtures extends Fixture implements OrderedFixtureInterface
         $jobExpired->setEmail('job@example.com');
         $jobExpired->setExpiresAt(new \DateTime('-10 days'));
 
-        for ($i = 100; $i <= 130; $i++) {
-        $job = new Job();
-        $job->setCategory($manager->merge($this->getReference('category-programming')));
-        $job->setType('full-time');
-        $job->setCompany('Company ' . $i);
-        $job->setPosition('Web Developer');
-        $job->setLocation('Paris, France');
-        $job->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
-        $job->setHowToApply('Send your resume to lorem.ipsum [at] dolor.sit');
-        $job->setPublic(true);
-        $job->setActivated(true);
-        $job->setToken('job_' . $i);
-        $job->setEmail('job@example.com');
-
-        $manager->persist($job);
-        }
-
         $manager->persist($jobSensioLabs);
         $manager->persist($jobExtremeSensio);
         $manager->persist($jobExpired);
